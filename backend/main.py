29317f5bb05df743f033385bc86fd7b92da15984
Chedd1 <qtm190000@utdallas.edu>
2025-03-25 18:53:31 -0500

    Committing backend for GET database functionality
    Whoops I was in the wrong directory for the last commit

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,19 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from typing import List
+from typing import List, Optional
+from sqlmodel import SQLModel, Session, create_engine, Field, select
 
-class Fruit(BaseModel):
+# SQLModel database object representing a fruit
+# A SQLModel object essentially links database objects to object classes in the code
+class dbFruit(SQLModel, table=True):
+    id: Optional[int] = Field(primary_key=True, index=True)
     name: str
 
+# Non-database objects
+class Fruit(BaseModel):
+    name: str
 class Fruits(BaseModel):
     fruits: List[Fruit]
 
@@ -22,14 +29,34 @@ app.add_middleware(
     allow_headers = ['*'],
 )
 
+# Set up database
+DATABASE_URL = "postgresql://neondb_owner:PASS@ep-dawn-rice-aafg5mw4-pooler.westus3.azure.neon.tech/neondb?sslmode=require"
+engine = create_engine(DATABASE_URL, connect_args={})
+SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
 # Temporary in-memory database
 memory_db = { "fruits": [] }
 
-# GET endpoint that calls get_fruits(), returning all the fruits stored in memory_db
-# FastAPI automatically converts the python object into JSON
-@app.get("/fruits", response_model=Fruits)
-def get_fruits():
-    return Fruits(fruits=memory_db["fruits"])
+# GET endpoint that calls read_fruits(), executnig a GET query on the database
+# FastAPI automatically converts the returned python object into JSON
+@app.get("/fruits", response_model=list[dbFruit])
+def read_fruits(
+    skip: int = 0, limit: int = 10, session: Session = Depends(get_session)
+):
+    # Database query- it gets the dbFruit table
+    # Note: using dbFruit.name would get the name along with id (because id is the primary key)
+    statement = select(dbFruit)
+    
+    # This is how to use the offset and limit options:
+    # statement = select(dbFruit).offset(skip).limit(limit)
+
+    # Executes the query
+    fruits = session.exec(statement).all()
+    return fruits
 
 # POST endpoint that adds a fruit to the memory_db list
 # FastAPI verifies the parameter is of the right type
