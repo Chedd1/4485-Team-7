630fdb31b40308323f8cbfa4093f29f56249a7df
Avani <68566882+avanimehro@users.noreply.github.com>
2025-04-08 12:18:51 -0500

    Delete frontend/src/api.js

--- frontend/src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-// When porting to AWS and running on that server, set baseURL to its IP address
-const api = axios.create({
-    baseURL: "http://localhost:8000/"
-});
-
-// Export the Axios instance
-export default api;
